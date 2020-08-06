f6d1f50e7cda98be18f13cf6b74ff899493c229f
LZ <zhanli@siom.ac.cn>
2020-08-06 16:46:34 +0800

    修改登录方法

--- loginAt4am.bat
+++ loginAt4am.bat
@@ -3,5 +3,4 @@ echo off
 echo 连接wifi中
 netsh wlan connect name=SIOM
 echo 登录中
-python.exe ./python_autologin.py
-pause
+python.exe ./login-4am.py
---
+++ powershell方法/执行.bat
@@ -0,0 +1,4 @@
+CHCP 65001
+echo off
+powershell.exe -ExecutionPolicy RemoteSigned   -file ".\登陆.ps1"
+pause
